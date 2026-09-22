a1b10c356ad0e75fe7501f2a6c9bc43df269171a
Joseph Davitt <joseph.j.davitt@gmail.com>
2026-09-22 10:45:01 -0700

    add PowerUpManager scaffold

---
+++ Assets/src/TL3_Joe/PowerUpManager.cs
@@ -0,0 +1,11 @@
+using System.Numerics;
+
+public class PowerUpManager 
+{
+
+    public enum PowerUpType { SpeedBoost = 0, Shield = 1, Magnet = 2 }
+    public void SpawnPowerUp(Vector3 pos, PowerUpType type)
+    {
+        // Code here
+    }
+}
